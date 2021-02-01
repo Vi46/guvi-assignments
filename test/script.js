de43f3e5efa3b3bc4b34965d7fee23d1cbb399d2
Vishal Adithya S <77295495+Vi46@users.noreply.github.com>
2021-02-01 11:29:40 +0530

    Delete test directory

--- test/script.js
+++
@@ -1,8 +0,0 @@
-let button = document.getElementsByTagName('p')[0];
-
-function RBC (e) {
-  p.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-}
-
-p.addEventListener("click", RBC);
-window.onload = RBC();
